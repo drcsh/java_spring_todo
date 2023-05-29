76536e1efa4d9f6cfe8a8ea68bfbf286e9e7b212
drcsh <drcsh@DESKTOP>
2023-05-29 11:57:10 +0100

    Adding note entity

---
+++ todo/src/main/java/com/example/todo/Note.java
@@ -0,0 +1,39 @@
+package com.example.todo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Table;
+import javax.persistence.Id;
+
+
+@Entity
+@Table(name = "note")
+public class Note {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String title;
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    
+}
